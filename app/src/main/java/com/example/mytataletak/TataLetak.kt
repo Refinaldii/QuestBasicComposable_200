35763e63bcd9bbe481cd52671c66f57270b35416
Refinaldii <refinaldi9786@gmail.com>
2025-10-17 10:15:48 +0700

    Commit tataletakcolumn

--- app/src/main/java/com/example/mytataletak/TataLetak.kt
+++ app/src/main/java/com/example/mytataletak/TataLetak.kt
@@ -1,21 +1,18 @@
+// File: TataletakLayouts.kt
 package com.example.mytataletak
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text("Komponen1")
+        Text("Komponen2")
+        Text("Komponen3")
+    }
+}
