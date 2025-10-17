319b906926b636810f0662531ba309642c0af4f4
Refinaldii <refinaldi9786@gmail.com>
2025-10-17 10:30:15 +0700

    Commit TataletakBoxcolumrow

--- app/src/main/java/com/example/mytataletak/MainActivity.kt
+++ app/src/main/java/com/example/mytataletak/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         setcontent {}
         MyTataLetakTheme {
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                TataletakBoxColumnRow(
         }
 
     }
