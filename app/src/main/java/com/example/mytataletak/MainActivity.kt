3dfe913d7c07b11f41fe268936122524c2a54f1c
Refinaldii <refinaldi9786@gmail.com>
2025-10-17 10:31:41 +0700

    Commit perbaiki erorr

--- app/src/main/java/com/example/mytataletak/MainActivity.kt
+++ app/src/main/java/com/example/mytataletak/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.mytataletak
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,29 +14,23 @@ import com.example.mytataletak.ui.theme.MyTataLetakTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setcontent {}
-        MyTataLetakTheme {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                TataletakBoxColumnRow(
-                    modifier = Modifier.padding(innerPadding)
+        setContent {
+            MyTataLetakTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Panggil layout yang kamu buat
+                    TataletakBoxColumnRow(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
-
     }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewTataletakBoxColumnRow() {
     MyTataLetakTheme {
-        Greeting("Android")
+        TataletakBoxColumnRow()
     }
 }
